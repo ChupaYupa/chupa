280410a59b57cf8bfb2cf0f9de4e5f53a0a69f92
Toma <docsperj@gmail.com>
2020-01-29 12:34:30 +0300

    Updates

---
+++ precache-manifest.0d3c0e833b383ac58168b9b49127f621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7788e5e64df4348ebc3c6a355853fdae",
+    "url": "/chupa/index.html"
+  },
+  {
+    "revision": "da83e7258a73db3b759d",
+    "url": "/chupa/static/css/main.3e24ed0b.chunk.css"
+  },
+  {
+    "revision": "4476ec03dd9fe0e86ce7",
+    "url": "/chupa/static/js/2.6aa68160.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/chupa/static/js/2.6aa68160.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da83e7258a73db3b759d",
+    "url": "/chupa/static/js/main.ed4333e7.chunk.js"
+  },
+  {
+    "revision": "8bcc42aa2ec394311b7b",
+    "url": "/chupa/static/js/runtime-main.9e928ef4.js"
+  }
+]);
