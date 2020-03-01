cc88e54b5f740f3daf5f68bac8c11ba36ebe327c
Toma <docsperj@gmail.com>
2020-03-01 19:21:29 +0300

    form contacts

--- src/Cantacts.jsx
+++ src/Cantacts.jsx
@@ -2,30 +2,53 @@ import React from 'react';
 import styles from './Cantacts.module.css';
 import axios from "axios";
 
-debugger
-const form = ("submit", (e) => {
-  debugger
-  e.preventDefault();
-  debugger
-  axios.post("http://localhost:3010/submit", {
+// let name = (event) => (
+//   event.currentTarget.value
+// );
+// let email = (event) => (
+//   event.currentTarget.value
+// );
+// let message = (event) => (
+//   event.currentTarget.value
+// );
 
-    // Name: form.querySelector('#Name').value,
-    // Email: form.querySelector('#Email').value,
-    // message: form.querySelector('#message').value,
-  })
-    .then(() => { alert('Your message has been send') })
-});
+
+
+debugger
+
 
 
 function Cantacts() {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [message, setMessage] = React.useState('')
+
+  let changeName = (event) => (
+    setName(event.currentTarget.value)
+  );
+  let changeEmail = (event) => (
+    setEmail(event.currentTarget.value)
+  );
+  let changeMessage = (event) => (
+    setMessage(event.currentTarget.value)
+  );
+
+  const form = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3010/submit", {
+      name, email, message
+    })
+      .then(() => { alert('Your message has been send') })
+  };
+
   return (
     <div className={styles.cantacts}>
       <div className={styles.container}>
         <div className={styles.myCantact}>Contacts</div>
         <form className={styles.form}>
-          <input type='text' placeholder="Name" id='Name'></input>
-          <input type='text' size='40px' placeholder="Email" id='Email'></input>
-          <textarea rows="10" cols="60" id='message' placeholder="Your message"></textarea>
+          <input type='text' placeholder="Name" onChange={changeName}></input>
+          <input type='text' size='40px' placeholder="Email" onChange={changeEmail}></input>
+          <textarea rows="10" cols="60" onChange={changeMessage} placeholder="Your message"></textarea>
         </form>
         <button className={styles.send} onClick={form}>Submit</button>
       </div>
